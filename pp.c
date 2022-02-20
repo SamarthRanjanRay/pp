4ef010ed2bd15ca7ff8af21c20089209f1cc2ea6
Samarth Ranjan Ray <samarthranjanray@gmail.com>
2022-02-20 18:23:39 +0530

    final revisionn

--- pp.c
+++ pp.c
@@ -371,6 +371,7 @@ int c[2][2] = {{0,0},{0,0}};
 void main(int argc, char* argv[]) {
     int rank, size;
     MPI_Init(&argc, &argv);
+    MPI_Status status;
     MPI_Comm_rank(MPI_COMM_WORLD,&rank);
     MPI_Comm_size(MPI_COMM_WORLD,&size);
 
@@ -387,7 +388,7 @@ void main(int argc, char* argv[]) {
         MPI_Send(&c,BUFFER,MPI_INT,1,0,MPI_COMM_WORLD);
     }
     else if (rank == 1) {
-        MPI_Recv(&c,BUFFER,MPI_INT,0,0,MPI_COMM_WORLD);
+        MPI_Recv(&c,BUFFER,MPI_INT,0,0,MPI_COMM_WORLD,&status);
         //print array
     }
     MPI_Finalize();
